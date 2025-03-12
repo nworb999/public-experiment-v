c5c4ac3abb5dfc5cebd4ca72d6b0135b587bab29
nworb999 <emma.nworb@gmail.com>
2025-03-12 16:50:51 -0700

    Commiting initial notes

---
+++ main.py
@@ -0,0 +1,13 @@
+# context management
+# prompt formatter
+# decision pipeline
+# tensionmeter
+# .say() .do() .go()
+# plan act reflect
+# available actions
+# relationships
+# complexes/personal narrative
+# goals/motivations->tactics
+
+
+
